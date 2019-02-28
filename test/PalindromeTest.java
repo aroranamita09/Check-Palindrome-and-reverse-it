3476016d88e20543f0f05d6f63424db400299e0e
Namita <aroranamita33@gmail.com>
2019-02-28 15:21:08 +0530

    h

--- test/PalindromeTest.java
+++ test/PalindromeTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
- class PalindromeTest {
+public class PalindromeTest {
     Palindrome obj;
     @Before
     public void setUp() throws Exception {
